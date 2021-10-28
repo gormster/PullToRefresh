ab5a015b56a8ed663ac708dd851e38e3228a9b55
Morgan Harris <gormster@me.com>
2021-10-28 14:18:27 +1100

    Remove example test

--- Tests/PullToRefreshTests/PullToRefreshTests.swift
+++ Tests/PullToRefreshTests/PullToRefreshTests.swift
@@ -2,10 +2,5 @@ import XCTest
 @testable import PullToRefresh
 
 final class PullToRefreshTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        
-    }
+    // TODO: How do we test this package? What needs to be tested?
 }
